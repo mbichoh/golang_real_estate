d999997c42ab1b27b107779f58ded602bd583a18
mbichoh <nattymbichoh@gmail.com>
2020-04-14 08:47:38 +0300

    application struct for handling the web app dependencies

--- app/web/handlers.go
+++ app/web/handlers.go
@@ -3,12 +3,11 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 	"strconv"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
@@ -24,19 +23,19 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		log.Println(err.Error())
+		app.errorLog.Println(err.Error())
 		http.Error(w, "Internal Server Error 1", 500)
 		return
 	}
 
 	err = ts.Execute(w, nil)
 	if err != nil {
-		log.Println(err.Error())
+		app.errorLog.Println(err.Error())
 		http.Error(w, "Internal Server Error 2", 500)
 	}
 }
 
-func showEstate(w http.ResponseWriter, r *http.Request) {
+func (app *application) showEstate(w http.ResponseWriter, r *http.Request) {
 
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
@@ -47,7 +46,7 @@ func showEstate(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "The Estate in page ID %d", id)
 }
 
-func createEstate(w http.ResponseWriter, r *http.Request) {
+func (app *application) createEstate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
 		w.Header().Set("Allow", "POST")
 		http.Error(w, "Method not allowed", 405)
--- app/web/main.go
+++ app/web/main.go
@@ -7,23 +7,34 @@ import (
 	"os"
 )
 
+//aplication struct to hold the application wide dependencies for the web app
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
+
 func main() {
 
 	//add command-line flag to network address
-	addr := flag.String("addr",":4047","Http Network Address")
+	addr := flag.String("addr", ":4047", "Http Network Address")
 
 	flag.Parse()
 
 	//logs
-	infoLog 	:= log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime) //infomation log
-	errorLog 	:= log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile) //errors log
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)                  //infomation log
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile) //errors log
 
+
+	app := &application{
+		errorLog: errorLog,
+		infoLog: infoLog,
+	}
 
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/estate", showEstate)
-	mux.HandleFunc("/estate/create", createEstate)
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/estate", app.showEstate)
+	mux.HandleFunc("/estate/create", app.createEstate)
 
 	//adding static files
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
@@ -31,11 +42,10 @@ func main() {
 
 	//initialize a new http.Server struct
 	srv := &http.Server{
-		Addr: *addr,
+		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler: mux,
+		Handler:  mux,
 	}
-
 
 	infoLog.Printf("Go development server started: http://localhost%s", *addr)
 	err := srv.ListenAndServe()
